8f6c02600e600235e685f3a744bc27d0ff32830c
Charles Franca <charlesfranca@MacBook-Pro-de-Charles.local>
2018-07-21 10:54:44 -0300

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MoviesProvider } from '../../providers/movies/movies';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  movies:any = [];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    public navCtrl: NavController,
+    private moviesProvider: MoviesProvider
+  ) {
+    this.moviesProvider.getPopularMovies()
+      .then(movies => {
+        this.movies = movies;
+      });
   }
 
 }
---
+++ src/providers/movies/movies.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { resolveDefinition } from '../../../node_modules/@angular/core/src/view/util';
+
+/*
+  Generated class for the MoviesProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class MoviesProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello MoviesProvider Provider');
+  }
+
+  getPopularMovies() {
+    return new Promise((resolve, reject) => {
+      this.http.get("https://api.themoviedb.org/3/movie/popular?api_key=f6b9adefc9f1e328fc5a66a32f4a3354")
+        .subscribe(response => {
+          var data = response as any;
+          resolve(data.results);
+        }, error => {
+          reject(error);
+        });
+    })
+  }
+
+}
